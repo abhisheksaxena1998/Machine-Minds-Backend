7e9bfb7eb29b70ebed5b80a4dd600fa3a1b2fbf2
isadev97 <isa325697@gmail.com>
2023-08-12 23:48:04 +0530

    added and requirements.txt

--- server/views.py
+++ server/views.py
@@ -2,9 +2,22 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from PIL import Image
 import random
+import openai
+import requests
+from django.conf import settings
+openai.api_key = settings.OPEN_AI_KEY
+import matplotlib.pyplot as plt
+from io import BytesIO
+from django.http import HttpResponse, FileResponse
 
+def generate(text):
+    res = openai.Image.create(
+    	prompt=text,
+    	n=1,
+    	size="256x256",
+    )
+    return res["data"][0]["url"]
 
-@csrf_exempt
 def ResolveProductCategory(request):
     if request.method == "POST":
         image = request.FILES["image"]
@@ -18,5 +31,9 @@ def ResolveProductCategory(request):
         response_data = {"status": "success", "category": random.choice(response_list)}
         return JsonResponse(response_data)
     else:
-        response_data = {"status": "error", "message": "Invalid request method"}
-        return JsonResponse(response_data, status=400)
+        text = "batman with green arrow"
+        url = generate(text)
+        response = requests.get(url)
+        raw_image = BytesIO(response.content)
+        image = Image.open(BytesIO(response.content))
+        return FileResponse(raw_image, as_attachment=True, filename='output.png')
