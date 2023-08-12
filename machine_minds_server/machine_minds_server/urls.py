b9543867d84be51fc9d9ec0a1c18177f49961423
Abhishek <abhi123923@gmail.com>
2023-08-12 13:37:44 +0530

    added an API to return product category

--- machine_minds_server/machine_minds_server/urls.py
+++ machine_minds_server/machine_minds_server/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from server.views import ResolveProductCategory
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("product_category_api/", ResolveProductCategory),
 ]
